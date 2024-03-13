53cf7d89a7f94efe3f7805d7f0e9e03e0d1828fd
Drew0004 <drdrewgraphic@gmail.com>
2024-03-13 14:22:27 +0100

    Add Api Controller

---
+++ app/Http/Controllers/Api/ProjectController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Project;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::with('type', 'technologies')->paginate(1);
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects,
+        ]);
+    }
+
+    public function show(string $slug)
+    {
+        $projects = Project::where('slug', $slug)->firstOrFail();
+
+        return response()->json([
+            'success' => true,
+            'results' => $projects,
+        ]);
+    }
+}
